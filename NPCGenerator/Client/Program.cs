75a2eec78c1a8cdf4003cc7eb88b569cbf89bf12
Faraz Zamansani <34017258+farazzamansani@users.noreply.github.com>
2022-06-18 23:18:56 +1000

    Added PreRendering

--- NPCGenerator/Client/Program.cs
+++ NPCGenerator/Client/Program.cs
@@ -5,8 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NPCGenerator.Shared.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddBlazoredModal();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new WeatherGenerator());
--- NPCGenerator/Server/Program.cs
+++ NPCGenerator/Server/Program.cs
@@ -3,6 +3,8 @@ using System.Reflection.Metadata;
 using Blazored.Modal;
 using Microsoft.AspNetCore.ResponseCompression;
 using NPCGenerator.Server.Services;
+using NPCGenerator.Shared.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,9 @@ builder.Services.AddRazorPages();
 //builder.Services.AddScoped<IMyDependency, MyDependency>(); //In non server blazor this is the same as a singleton
 builder.Services.AddTransient<IMyDependency, MyDependency>();
 builder.Services.AddSingleton<IGenerator, Generator>();
+builder.Services.AddScoped(sp => new WeatherGenerator());
+builder.Services.AddScoped(sp => new HttpClient());
+
 builder.Services.AddBlazoredModal();
 
 var app = builder.Build();
@@ -40,7 +45,7 @@ app.UseRouting();
 
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapFallbackToPage("/_Host");
 
 app.Run();
 
